9321feda74d771873793da9cd47ea79fe69b4922
SoyNegro <krisoul.valdes@gmail.com>
2023-06-28 17:21:50 -0400

    Given two strings s1,s2 return true if s2 is an anagram of s1, or false otherwise

--- src/main/java/Easy/Classic.java
+++ src/main/java/Easy/Classic.java
@@ -4,13 +4,25 @@ public class Classic {
     public static void main(String[] args) {
         System.out.println(isPalindrome("())("));
         System.out.println(isPalindrome("lolo"));
+
+        System.out.println(isAnagram("cuba","bacu"));
+        System.out.println(isAnagram("bacu","acu"));
     }
     /**
      * Classic easy problem
-     * Given a String s return <code>true</code> if s is palindrome or <code>false</code> otherwise
+     * Given a String s return true if s is palindrome or false otherwise
      * */
     static boolean isPalindrome(String s){
         var reverse =  new StringBuilder(s).reverse().toString();
         return s.equals(reverse);
+    }
+
+    /**
+     * Classic easy problem
+     * Given two strings s1,s2 return true if s2 is an anagram of s1, or false otherwise
+     * */
+    static boolean isAnagram(String s1, String s2){
+        if(s1.length()!=s2.length()) return false;
+        return s1.chars().sum() == s1.chars().sum();
     }
 }
